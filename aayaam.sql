681939c877182d9390dfef31ba5bbf84abc15dfc
Pradeep <pradeepsng30@yahoo.com>
2015-06-29 18:58:39 +0530

    sql db added

--- aayaam.sql
+++ aayaam.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 26, 2015 at 11:13 PM
+-- Generation Time: Jun 29, 2015 at 03:16 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.5.24
 
@@ -23,6 +23,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(10) NOT NULL,
+  `name` text NOT NULL,
+  `username` text NOT NULL,
+  `posthead` text NOT NULL,
+  `posttext` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `verified` tinyint(1) NOT NULL DEFAULT '0',
+  `imageurl` text
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 COMMENT='table for posts';
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `name`, `username`, `posthead`, `posttext`, `timestamp`, `verified`, `imageurl`) VALUES
+(14, 'test1', 'test1@gmail.com', 'my head', '\r\npost\r\nlosl\r\nksks\r\nssjs\r\nsjsjssjsjssjjssjs\r\npost\r\nlosl\r\nksks\r\nssjs\r\nsjsjssjsjssjjssjs\r\npost\r\nlosl\r\nksks\r\nssjs\r\nsjsjssjsjssjjssjs\r\npost\r\nlosl\r\nksks\r\nssjs\r\nsjsjssjsjssjjssjs\r\npost\r\nlosl\r\nksks\r\nssjs\r\nsjsjssjsjssjjssjs\r\npost\r\nlosl\r\nksks\r\nssjs\r\nsjsjssjsjssjjssjs', '2015-06-29 11:12:24', 0, 'upload/img_2015-06-29_1435576344'),
+(15, 'pradeep', 'pradeep@gmail.com', 'pradeep', 'post\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\npost\r\n', '2015-06-29 11:52:51', 0, 'upload/img_2015-06-29_1435578771'),
+(16, 'pradeep', 'pradeep@gmail.com', 'haha', 'teting testing', '2015-06-29 11:55:45', 0, '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `userslogin`
 --
 
@@ -33,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `userslogin` (
   `password` text NOT NULL,
   `emailverified` tinyint(1) NOT NULL DEFAULT '0',
   `verified` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COMMENT='table for login, email verification and member verificatio';
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COMMENT='table for login, email verification and member verificatio';
 
 --
 -- Dumping data for table `userslogin`
@@ -44,11 +70,21 @@ INSERT INTO `userslogin` (`id`, `name`, `username`, `password`, `emailverified`,
 (2, 'hi', 'ji', 'i', 0, 0),
 (3, 'hi', 'ji', 'i', 0, 0),
 (4, 'test1', '', 'd41d8cd98f00b204e9800998ecf8427e', 0, 0),
-(5, 'test1', 'test1@gmail.com', '5a105e8b9d40e1329780d62ea2265d8a', 0, 0);
+(5, 'test1', 'test1@gmail.com', '5a105e8b9d40e1329780d62ea2265d8a', 0, 0),
+(6, 'pradeepsng30@gmail.com', 'pradeepsng30@gmail.com', 'root2', 0, 0),
+(7, 'pradeep', 'p@gmail.com', 'febc8f8ac083f5fc27e032c81e7b536a', 0, 0),
+(8, 'pradeep', 'p@gmail.com', 'febc8f8ac083f5fc27e032c81e7b536a', 0, 0),
+(9, 'pradeep', 'pradeep@gmail.com', 'febc8f8ac083f5fc27e032c81e7b536a', 0, 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `userslogin`
@@ -61,10 +97,15 @@ ALTER TABLE `userslogin`
 --
 
 --
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+--
 -- AUTO_INCREMENT for table `userslogin`
 --
 ALTER TABLE `userslogin`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
